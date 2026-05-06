9c8181adf1e8074fa4a94b88ba28bb467b85d395
nicolasholanda <nikolas.nhm14@gmail.com>
2026-05-06 08:35:50 -0300

    Initial commit

---
+++ src/main/java/com/converter/ComplexTypeConverterApplication.java
@@ -0,0 +1,12 @@
+package com.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexTypeConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplexTypeConverterApplication.class, args);
+    }
+}
